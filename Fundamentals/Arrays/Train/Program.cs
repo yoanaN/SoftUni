a7910acad6a5c55a1700d5677141b8accca60361
yoanaN <91801674+yoanaN@users.noreply.github.com>
2022-09-17 23:10:42 +0300

    Add files via upload

---
+++ Fundamentals/Arrays/Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] passengers=new int[n];
+            int sum = 0;
+            for(int i = 0; i <passengers.Length; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                sum += passengers[i];
+            }
+
+            for(int j = 0; j <passengers.Length; j++)
+            {
+                Console.Write($"{passengers[j]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
