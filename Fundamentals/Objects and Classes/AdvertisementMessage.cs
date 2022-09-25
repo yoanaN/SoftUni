f7ab78babeced968385caa8b33823e5b0211bee7
yoanaN <91801674+yoanaN@users.noreply.github.com>
2022-09-25 23:45:14 +0300

    Add files via upload

---
+++ Fundamentals/Objects and Classes/AdvertisementMessage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+namespace ObjectClasses
+{
+    internal class AdvertisementMessage
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string[] Phrases = new string[] { "Now I feel good.",
+                "I have succeeded with this product.",
+            "Makes miracles. I am happy of the results!",
+            "I cannot believe but now I feel awesome.",
+            "Try it yourself, I am very satisfied.",
+            "I feel great!"};
+
+            string[] Events = new string[] { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] Authors = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+            string[] Cities = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random num = new Random();
+            for (int i = 0; i < number; i++)
+            {
+                int PhraseIndex = num.Next(0, Phrases.Length);
+                int EventIndex = num.Next(0, Events.Length);
+                int AuthorIndex = num.Next(0, Authors.Length);
+                int CityIndex = num.Next(0, Cities.Length);
+
+                Console.WriteLine($"{Phrases[PhraseIndex]} { Events[EventIndex]} {Authors[AuthorIndex]}-{Cities[CityIndex]}");
+            }
+
+        }
+    }
+}
---
+++ Fundamentals/Objects and Classes/OrderbyAge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderByAge2
+{
+    class Person
+    {
+        public Person(string name, string id, int Age)
+        {
+            this.Name = name;
+            this.ID = id;
+            this.Age = Age;
+        }
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+    }
+    class OrderbyAge
+    {
+        static void Main(string[] args)
+        {
+            List<Person> allPerson = new List<Person>();
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                string[] info = input.Split();
+                string name = info[0];
+                string Id = info[1];
+                int age = int.Parse(info[2]);
+                Person person = new Person(name, Id, age);
+                Person person2 = allPerson.FirstOrDefault(x => x.ID == Id);
+                if (person2 == null)
+                {
+                    allPerson.Add(person);
+                }
+                else
+                {
+                    person2.Name = name;
+                    person2.Age = age;
+                    person2.ID = Id;
+                }
+                input = Console.ReadLine();
+            }
+            allPerson = allPerson.OrderBy(x => x.Age).ToList();
+
+            foreach (Person person in allPerson)
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+            }
+        }
+    }
+}
