c0d6c8d070d1408270b41c029b03499e09669091
yoanaN <91801674+yoanaN@users.noreply.github.com>
2022-09-28 23:54:24 +0300

    Create StudentAcademy.cs

---
+++ Fundamentals/Associative Arrays/StudentAcademy.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+                students[name].Add(grade);
+            }
+            foreach (var kvp in students.Where(x => x.Value.Average(x => x) >= 4.50))
+            {
+                Console.WriteLine($"{kvp.Key} -> {(kvp.Value.Average(x => x)):f2}");
+            }
+        }
+    }
+}
