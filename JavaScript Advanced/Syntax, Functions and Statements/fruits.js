9d1f17888dc6f91986110e371d776f6be691c326
yoanaN <91801674+yoanaN@users.noreply.github.com>
2022-10-02 23:04:59 +0300

    Create fruits.js

---
+++ JavaScript Advanced/Syntax, Functions and Statements/fruits.js
@@ -0,0 +1,6 @@
+function fruit(type,weight,price){
+    let weightInKg=weight/1000;
+    let totalMoney = weightInKg*price;
+    console.log(`I need $${totalMoney.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${type}.`);
+}
+fruit('apple', 1563, 2.35);
