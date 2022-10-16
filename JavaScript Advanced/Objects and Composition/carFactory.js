4cfd3626c2460eb5f6911646897c728357d6914d
yoanaN <91801674+yoanaN@users.noreply.github.com>
2022-10-16 12:33:09 +0300

    Add files via upload

---
+++ JavaScript Advanced/Objects and Composition/carFactory.js
@@ -0,0 +1,46 @@
+function carfactory(data){
+    let res={};
+    res.model=data.model;
+    let engineEnum={
+        "Small engine": { power: 90, volume: 1800 },
+        "Normal engine": { power: 120, volume: 2400 },
+        "Monster engine": { power: 200, volume: 3500 }
+    }
+    let carriageEnum={
+        hatchback:function(color){
+            return{
+                type:"hatchback",
+                color
+            }
+        },
+        coupe:function(color){
+            return{
+                type:"coupe",
+                color
+            }
+        }    
+        
+
+    }
+
+    if(data.power<=90){
+        res.engine=engineEnum["Small engine"];
+    }else if(data.power<=120){
+        res.engine=engineEnum["Normal engine"];
+    }else{
+        res.engine=engineEnum["Monster engine"];
+    }
+
+   res["carriage"]=carriageEnum[data.carriage](data.color);
+    let size=data.wheelsize%2===0?data.wheelsize-1:data.wheelsize;
+    
+    res.wheels=new Array(4).fill(size);
+    console.table(res);
+    return res;
+
+}
+carfactory({ model: 'VW Golf II',
+power: 90,
+color: 'blue',
+carriage: 'hatchback',
+wheelsize: 14 })
